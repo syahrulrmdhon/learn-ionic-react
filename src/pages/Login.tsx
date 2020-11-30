ccdda3ce447cfbd447938ca331ac53c0ed92c7a9
syahrulrmdhon <syahrulrmdhon2@gmail.com>
2020-11-30 11:02:31 +0700

    add pages login and register

---
+++ src/pages/Login.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { IonGrid, IonRow, IonCol } from "@ionic/react";
+import { useHistory } from "react-router-dom";
+import {
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonButton,
+  IonAlert,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import Logo from '../image/makNewss.png';
+
+const Login: React.FC = () => {
+  const history = useHistory();
+  const handleLogin = () => {
+    return history.push("/home");
+  };
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login Mak News</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen className="ion-padding ion-text-center">
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonAlert
+                isOpen={false}
+                onDidDismiss={() => {
+                  console.log("yes");
+                }}
+                cssClass="my-custom-class"
+                header={"Error!"}
+                buttons={["Dismiss"]}
+              />
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <img style={{ width: '100px' }} src={Logo} />
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="floating"> Email</IonLabel>
+                <IonInput type="email"></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="floating"> Password</IonLabel>
+                <IonInput type="password"></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <p style={{ fontSize: "small" }}>
+                By clicking LOGIN you agree to our <a href="#">Policy</a>
+              </p>
+              <IonButton expand="block" onClick={handleLogin}>
+                Login
+              </IonButton>
+              <p style={{ fontSize: "medium" }}>
+                Don't have an account? <a href="/signup">Sign up!</a>
+              </p>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
